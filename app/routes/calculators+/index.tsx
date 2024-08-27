d575bba6c5e537474ce6e2efa71a153fed8533e4
Daniel Kapper <dkapper01@gmail.com>
2024-08-27 15:09:55 +0300

    update calculator list

--- app/routes/calculators+/index.tsx
+++ app/routes/calculators+/index.tsx
@@ -3,18 +3,23 @@ import { Link } from '@remix-run/react'
 const listOfCalculators = [
 	{ name: 'Morgage Loan', path: 'morgage-loan' },
 	{ name: 'Rental Property', path: 'rental-property' },
-	{ name: 'BRRRR', path: 'brrrr' },
-	{ name: 'Fix and Flip', path: 'fix-and-flip' },
+	// { name: 'BRRRR', path: 'brrrr' },
+	// { name: 'Fix and Flip', path: 'fix-and-flip' },
 	{ name: 'Rehab Cost Estimate', path: 'rehab-cost-estimate' },
 ]
 export default function Calculators() {
 	return (
-		<div>
-			Calculators
-			<ul>
+		<div className="mx-auto max-w-2xl p-6">
+			<h1 className="mb-6 text-3xl font-bold">Calculators</h1>
+			<ul className="space-y-3">
 				{listOfCalculators.map((calculator) => (
 					<li key={calculator.name}>
-						<Link to={calculator.path}>{calculator.name}</Link>
+						<Link
+							to={calculator.path}
+							className="block rounded bg-blue-100 px-4 py-3 font-semibold text-blue-800 transition duration-300 ease-in-out hover:bg-blue-200"
+						>
+							{calculator.name}
+						</Link>
 					</li>
 				))}
 			</ul>
