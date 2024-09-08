60f386b1566333c881db6e17ffe65af6285b5c55
Daniel Kapper <dkapper01@gmail.com>
2024-09-08 10:58:25 -0400

    fix color issue not showing

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,11 +2,11 @@ import { type Config } from 'tailwindcss'
 import animatePlugin from 'tailwindcss-animate'
 import radixPlugin from 'tailwindcss-radix'
 import { marketingPreset } from './app/routes/_marketing+/tailwind-preset'
-// import { extendedTheme } from './app/utils/extended-theme.ts'
+import { extendedTheme } from './app/utils/extended-theme.ts'
 
 export default {
 	content: ['./app/**/*.{ts,tsx,jsx,js}'],
-	darkMode: ['class', 'class'],
+	darkMode: 'class',
 	theme: {
 		container: {
 			center: true,
@@ -15,30 +15,7 @@ export default {
 				'2xl': '1400px',
 			},
 		},
-		extend: {
-			keyframes: {
-				'accordion-down': {
-					from: {
-						height: '0',
-					},
-					to: {
-						height: 'var(--radix-accordion-content-height)',
-					},
-				},
-				'accordion-up': {
-					from: {
-						height: 'var(--radix-accordion-content-height)',
-					},
-					to: {
-						height: '0',
-					},
-				},
-			},
-			animation: {
-				'accordion-down': 'accordion-down 0.2s ease-out',
-				'accordion-up': 'accordion-up 0.2s ease-out',
-			},
-		},
+		extend: extendedTheme,
 	},
 	presets: [marketingPreset],
 	plugins: [animatePlugin, radixPlugin],
